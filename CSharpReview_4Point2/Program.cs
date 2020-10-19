c0faee8c45bceba2b9c2af16a436f866ea65f13c
James Grieve <jamesonrgrieve@gmail.com>
2020-10-19 09:59:48 -0600

    Data types;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -8,6 +8,21 @@ namespace CSharpReview_4Point2
         // Entry point of programs in C#.
         {
             // Declaration of a variable.
+            char aChar; // A single character value.
+            string aString; // String (collection of characters) values.
+
+            bool aBoolean; // Boolean (true-false) values (bit).
+
+            byte aByte; // Super small integer values (~120 max).
+            short aShortInt; // Small integer values (~65000 max).
+            int anInt; // Integer values (~210000000 max).
+            long aLongInt; // BIG integer values (9223372036854775807 max).
+
+            float aFloat; // Floating point values (less precision than doubles).
+            double aDouble; // Decimal values (like a float but more precise).
+            decimal aDecimal; // Also decimal values (extremely precise).
+
+
             string name;
 
             // Input from the console.
