ba45975ae4c63f5c30d7dac813b29d10d09b3ba3
James Grieve <jamesonrgrieve@gmail.com>
2020-10-19 10:50:09 -0600

    While and for loops;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -29,32 +29,27 @@ namespace CSharpReview_4Point2
             int userInput;
             string output;
 
-            Console.Write("Please enter a number: ");
+            Console.Write("Please enter a number to count to: ");
 
             // int.Parse is "dangerous" because it can throw an exception.
             userInput = int.Parse(Console.ReadLine());
 
-            // Switch works with discrete values, not ranges like if allows, but some people find it more readable.
-            // Anything* you can do with a switch, you can do with an if/else.
-            // * Falling through cases is questionable with if/else as you need some other logic, but it's questionable whether
-            // that should be done anyways.
 
-            switch (userInput % 2)
+            // Initialize, Test, Process, Modify
+
+
+            for (int x = 1 /* Initialize */ ; x <= userInput /* Test  */; x++ /* Modify */)
             {
-                case 0:
-                    output = "even";
-                    break;
-                case 1:
-                    output = "odd";
-                    break;
-                default:
-                    output = "error";
-                    break;
+                // Process
+                Console.WriteLine(x);
             }
-
-            Console.WriteLine($"The number entered is {userInput}, it is {output}.");
-
-
+       
+            int y = 1; // Initialize
+            while (y <= userInput) // Test
+            {
+                Console.WriteLine(y); // Process
+                y++; //Modify
+            }
             // Exit point of programs in C#.
         }
     }
