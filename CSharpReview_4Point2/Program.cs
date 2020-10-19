f64d8af9ffe525937903cbb27c04078e1b06e723
James Grieve <jamesonrgrieve@gmail.com>
2020-10-19 09:36:32 -0600

    Changed output in Program.cs;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -6,7 +6,7 @@ namespace CSharpReview_4Point2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! What a nice day!");
         }
     }
 }
