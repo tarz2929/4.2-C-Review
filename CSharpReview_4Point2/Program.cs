27526b19c91d9869e2541e54f9e577e96ef7db60
James Grieve <jamesonrgrieve@gmail.com>
2020-10-19 09:52:50 -0600

    Console input and output; Variables;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -5,8 +5,18 @@ namespace CSharpReview_4Point2
     class Program
     {
         static void Main(string[] args)
+        // Entry point of programs in C#.
         {
+            // Declaration of a variable.
+            string name;
+
+            // Input from the console.
+            name = Console.ReadLine();
+
+            // Output to the console.
             Console.WriteLine("Hello World! What a nice day today!");
+
+        // Exit point of programs in C#.
         }
     }
 }
