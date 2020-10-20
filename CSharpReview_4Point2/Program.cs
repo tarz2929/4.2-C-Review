6e586e807f68afa29a79b2127b8fc2ffb83ae13f
James Grieve <jamesonrgrieve@gmail.com>
2020-10-20 09:21:58 -0600

    Methods / functions;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -21,5 +21,22 @@ namespace CSharpReview_4Point2
                 Console.WriteLine(intArray[i]);
             }
         }
+
+        /*
+            static: Tells C# to only keep one copy of the method in memory (important for OOP).
+            int: Return type, the type of data coming out of the method.
+            GetInt: Name of the method, how we call it.
+            string: First parameter type, the type of input expected.
+            prompt: First parameter/argument name, how we refer to that value in the method.
+        */
+        static int GetInt(string prompt)
+        {
+            int input;
+
+            Console.Write(prompt);
+            input = int.Parse(Console.ReadLine());
+
+            return input;
+        }
     }
 }
