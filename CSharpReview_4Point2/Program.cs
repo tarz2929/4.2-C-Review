7fd7a9b32885ecaa4d2041271f026426a7055b95
James Grieve <jamesonrgrieve@gmail.com>
2020-10-20 09:25:12 -0600

    Method usage;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -7,19 +7,9 @@ namespace CSharpReview_4Point2
         static void Main(string[] args)
         // Entry point of programs in C#.
         {
-            // Arrays in C# are fixed-length collections of items.
+            int age = GetInt("Please enter your age: ");
 
-            // Initialize an array of length 5.
-            // The first element will have index 0, second index 1, up to fifth index 4.
-            int[] intArray = new int[] { 5, 3, 10, 12, 42 };
-
-            // If we want to set it, just like most other languages, we can set an element by assigning an index.
-            intArray[3] = 10;
-
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                Console.WriteLine(intArray[i]);
-            }
+            int[] intArray = new int[GetInt("Please enter a number of integers to store: ")];
         }
 
         /*
