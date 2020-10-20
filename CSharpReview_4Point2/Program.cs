1ce8add5d01bf801e990ea0b36b6ae002300cf9d
James Grieve <jamesonrgrieve@gmail.com>
2020-10-20 09:17:57 -0600

    Arrays;

--- CSharpReview_4Point2/Program.cs
+++ CSharpReview_4Point2/Program.cs
@@ -7,60 +7,19 @@ namespace CSharpReview_4Point2
         static void Main(string[] args)
         // Entry point of programs in C#.
         {
-            // In-Class Exercise: 
-            // Make the "Admin" decision branch case-insensitive.
-            
-            // Declaration of a variable.
-            char aChar; // A single character value.
-            string aString; // String (collection of characters) values.
+            // Arrays in C# are fixed-length collections of items.
 
-            bool aBoolean; // Boolean (true-false) values (bit).
+            // Initialize an array of length 5.
+            // The first element will have index 0, second index 1, up to fifth index 4.
+            int[] intArray = new int[] { 5, 3, 10, 12, 42 };
 
-            byte aByte; // Super small integer values (~120 max).
-            short aShortInt; // Small integer values (~65000 max).
-            int anInt; // Integer values (~210000000 max).
-            long aLongInt; // BIG integer values (9223372036854775807 max).
+            // If we want to set it, just like most other languages, we can set an element by assigning an index.
+            intArray[3] = 10;
 
-            float aFloat; // Floating point values (less precision than doubles).
-            double aDouble; // Decimal values (like a float but more precise).
-            decimal aDecimal; // Also decimal values (extremely precise).
-
-
-            int userInput;
-            string output;
-
-            Console.Write("Please enter a number to count to: ");
-
-            // int.Parse is "dangerous" because it can throw an exception.
-            userInput = int.Parse(Console.ReadLine());
-
-
-            // Initialize, Test, Process, Modify
-
-            // Pre-Test Loops. Do not run at all if the condition is false.
-            for (int x = 1 /* Initialize */ ; x <= userInput /* Test  */; x++ /* Modify */)
+            for (int i = 0; i < intArray.Length; i++)
             {
-                // Process
-                Console.WriteLine(x);
+                Console.WriteLine(intArray[i]);
             }
-       
-            int y = 1; // Initialize
-            while (y <= userInput) // Test
-            {
-                Console.WriteLine(y); // Process
-                y++; //Modify
-            }
-
-
-            // Post-Test Loop. Always runs at least once, even if the condition is false.
-            int z = 1; // Initialize
-            do
-            {
-                Console.WriteLine(z); // Process
-                z++; // Modify
-            }
-            while (z <= userInput); // Test
-            // Exit point of programs in C#.
         }
     }
 }
